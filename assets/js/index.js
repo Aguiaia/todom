754c9825ae551118c2cb0460f431cfef937bc667
Aguiaia <aguiaia.ximenes@gmail.com>
2020-05-13 17:36:33 -0300

    Todo list quase pronta

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,20 +6,20 @@ let tarefas = [{
     },
     {
         id: 2,
-        texto: "Dormir",
-        prioridade: 3,
+        texto: "Gravar realização de desafio",
+        prioridade: 1,
         feito: true
     },
     {
         id: 3,
-        texto: "Estudar",
-        prioridade: 1,
+        texto: "Fazer almoço",
+        prioridade: 2,
         feito: false
     },
     {
         id: 4,
-        texto: "Ler",
-        prioridade: 2,
+        texto: "Pagar escolas",
+        prioridade: 3,
         feito: false
     }
 ]
@@ -30,17 +30,15 @@ const render = (tarefas) => {
     let table = document.getElementById("table");
 
     // Alternativa para o document.getElementById("table");
-    // table = document.querySelector(".azul div");
+    // table = document.querySelector("#table");
 
     // Limpar a lista
-    table.innerHTML = "";
-    // Essa tag tem um irmãozinho que é o textContent, o qual não processa HTML. 
-    // Temos também o innerText.
+    table.innerText = "";
 
     // Criando a lista de tarefas
     for (const tarefa of tarefas) {
 
-        // Criando
+        // Criando uma linha de tabela
         let row = document.createElement('tr');
 
         // Criar o input checkbox
@@ -51,8 +49,8 @@ const render = (tarefas) => {
         let tdCheck = document.createElement('td');
         tdCheck.appendChild(checkbox);
 
-        // Adicionar esse tdCheckbox à row
-        row.appendChild(tdCheck)
+        // Adicionar esse tdCheck à row
+        row.appendChild(tdCheck);
 
         // Criar a td de texto
         let tdTexto = document.createElement('td');
@@ -67,18 +65,23 @@ const render = (tarefas) => {
         tdAcoes.appendChild(i);
         row.appendChild(tdAcoes);
 
-        // Adicionar conteúdo dessa linha
+        // Adicionar a linha a tabela
         table.appendChild(row);
+
     }
+
 }
 
-// Criar função create (texto, prioridade) que recebe um texto  e prioridade como parâmetros
-// Essa função deve retornar um objeto literal com os seguintes campos
-// texto: com o texto passado por parâmetro
-// prioridade: com base no texto passado por parâmetro
-// feito: false
+/**
+ * Criar função create(texto,prioridade) que recebe um texto e prioridade como parâmetros
+ * Essa função deve retornar um objeto literal com os seguintes campos
+ * texto: com o texto passado por parâmetro
+ * prioridade: com base na prioridade passada como parâmetro
+ * feito: false
+ */
 const create = (texto, prioridade) => {
-    let tarefa = {
+    return {
+        id: tarefas[tarefas.length - 1].id + 1,
         texto,
         prioridade,
         feito: false
@@ -86,16 +89,59 @@ const create = (texto, prioridade) => {
 }
 
 
-let form = document.getElementById('form'); //Capturar o form
-// FORMA 1 = = = = = = = = = = = = = = = 
-// form.onsubmit = () => (evt) => {
-//     evt.prevetDefault("teste");
+// Capturar elementos importantes da página
+let form = document.getElementById('form'); // Capturar o form
+
+// FORMA 1 = = = = = = = = = = = =
+// form.onsubmit = (evt) => {
+//     console.log("teste");
 // }
 
-// FORMA 2 = = = = = = = = = = = = = = =
-form.addEventListener('submit', () => {
-    evt.prevetDefault("teste");
-    console.log("teste");
+// FORMA 2 = = = = = = = = = = = =
+form.addEventListener('submit', (evt) => {
+
+    // Evitar o comportamento padrão de um form
+    evt.preventDefault();
+
+    // Capturar o texto digitado pelo usuário
+    let texto = document.getElementById("tf_2do").value;
+
+    // Verificar se existe prioridade settada nesse texto
+    let strInicio = texto.substr(0, 3);
+    let prioridade;
+    switch (strInicio) {
+        case '#1 ':
+            prioridade = 1;
+            texto = texto.slice(3);
+            break;
+
+        case '#2 ':
+            prioridade = 2;
+            texto = texto.slice(3);
+            break;
+
+        case '#3 ':
+            prioridade = 3;
+            texto = texto.slice(3);
+            break;
+
+        default:
+            prioridade = 1;
+            break;
+    }
+
+    // Criar o objeto de tarefa sabendo o texto e a prioridade
+    let tarefa = create(texto, prioridade);
+
+    // Adicionar o objeto tarefa ao array de tarefas
+    tarefas.push(tarefa);
+
+    // Renderizar a minha lista novamente
+    render(tarefas);
+
+    // Lipar o campo de texto
+    document.getElementById("tf_2do").value = "";
+
 });
 
 render(tarefas);
